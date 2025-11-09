1ddb735466926bb25d6665995efea4ecb51cb821
Manav Pathania <aniketsarraf.in@gmail.com>
2025-11-09 20:55:25 +0530

    Update vite.config.js
    update due to security

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,9 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
-import dotenv from 'dotenv';
 
-dotenv.config();
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,24 +10,4 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
-  define: {
-    'process.env': process.env,
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-  server: {
-    port: 5173,
-    open: true,
-    proxy: {
-      '/api': {
-        target: 'https://kalawatiputra-edu.vercel.app/' || 'http://localhost:5000',
-        changeOrigin: true,
-        secure: false,
-      },
-
-    }
-  },
 });
